3a340d914c562f1410e2d0e395af1035af18fe29
nswbmw <gxqzk@126.com>
2018-05-14 13:50:56 +0800

    update

--- swift-pageMenu-demo/swift-pageMenu-demo/ViewController.swift
+++ swift-pageMenu-demo/swift-pageMenu-demo/ViewController.swift
@@ -11,53 +11,39 @@ import PageMenu
 
 class ViewController: UIViewController {
     
-    var pageMenu : CAPSPageMenu?
+  var pageMenu : CAPSPageMenu?
     
-  override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        // MARK: - UI Setup
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-        self.title = "PAGE MENU"
-        self.navigationController?.navigationBar.barTintColor = UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-    self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-    self.navigationController?.navigationBar.barStyle = UIBarStyle.black
-    self.navigationController?.navigationBar.tintColor = UIColor.white
-    self.navigationController?.navigationBar.titleTextAttributes = [kCTForegroundColorAttributeName as NSAttributedStringKey: UIColor.orange]
-        
-        // MARK: - Scroll menu setup
-        
-        // Initialize view controllers to display and place in array
-        var controllerArray: [UIViewController] = []
+    // Initialize view controllers to display and place in array
+    var controllerArray: [UIViewController] = []
         
     let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
     let controller1 = mainStoryboard.instantiateViewController(withIdentifier: "myTableView")
-        controller1.title = "First"
-        controllerArray.append(controller1)
+    controller1.title = "哈哈"
+    controllerArray.append(controller1)
         
     let controller2 = mainStoryboard.instantiateViewController(withIdentifier: "myTableView")
-        controller2.title = "Second"
-        controllerArray.append(controller2)
-        
-        // Customize menu (Optional)
-        let parameters: [CAPSPageMenuOption] = [
-          .scrollMenuBackgroundColor(UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)),
-          .viewBackgroundColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 1.0)),
-          .selectionIndicatorColor(UIColor.orange),
-            .bottomMenuHairlineColor(UIColor(red: 70.0/255.0, green: 70.0/255.0, blue: 80.0/255.0, alpha: 1.0)),
-            .menuItemFont(UIFont(name: "HelveticaNeue", size: 13.0)!),
-            .menuHeight(40.0),
-            .menuItemWidth(90.0),
-            .centerMenuItems(true)
-        ]
-        
-        // Initialize scroll menu
-    pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRect(x: 0.0, y: 0.0, width: self.view.frame.width, height: self.view.frame.height), pageMenuOptions: parameters)
-        
-        self.addChildViewController(pageMenu!)
-        self.view.addSubview(pageMenu!.view)
-        
-    pageMenu!.didMove(toParentViewController: self)
-    }
+    controller2.title = "呵呵"
+    controllerArray.append(controller2)
+    
+    // Customize menu (Optional)
+    let parameters: [CAPSPageMenuOption] = [
+      .scrollMenuBackgroundColor(UIColor.white),
+      .viewBackgroundColor(UIColor.white),
+      .selectionIndicatorColor(UIColor.orange),
+      .selectedMenuItemLabelColor(UIColor.black),
+      .bottomMenuHairlineColor(UIColor.lightGray),
+      .menuItemFont(UIFont(name: "HelveticaNeue", size: 17.0)!),
+      .menuHeight(44.0),
+      .menuItemWidth(34.0),
+      .centerMenuItems(true)
+    ]
+    
+    // Initialize scroll menu
+    pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRect(x: 0.0, y: 20.0, width: self.view.frame.width, height: self.view.frame.height), pageMenuOptions: parameters)
+    
+    self.view.addSubview(pageMenu!.view)
+  }
 }

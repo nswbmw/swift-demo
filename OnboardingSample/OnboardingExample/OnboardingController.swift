d06d88a23cef02a87ca7c20836106ac41266f6b9
nswbmw <gxqzk@126.com>
2019-02-01 14:33:08 +0800

    add: OnboardingSample

---
+++ OnboardingSample/OnboardingExample/OnboardingController.swift
@@ -0,0 +1,85 @@
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!{
+        didSet{
+            scrollView.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
+    var slides:[Slide] = [];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        slides = createSlides()
+        setupSlideScrollView(slides: slides)
+        
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        view.bringSubview(toFront: pageControl)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
+    func createSlides() -> [Slide] {
+
+        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide1.imageView.image = UIImage(named: "ic_onboarding_1")
+        slide1.labelTitle.text = "A real-life bear"
+        slide1.labelDesc.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide2.imageView.image = UIImage(named: "ic_onboarding_2")
+        slide2.labelTitle.text = "A real-life bear"
+        slide2.labelDesc.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+        let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide3.imageView.image = UIImage(named: "ic_onboarding_3")
+        slide3.labelTitle.text = "A real-life bear"
+        slide3.labelDesc.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+        let slide4:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide4.imageView.image = UIImage(named: "ic_onboarding_4")
+        slide4.labelTitle.text = "A real-life bear"
+        slide4.labelDesc.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+        
+        let slide5:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide5.imageView.image = UIImage(named: "ic_onboarding_5")
+        slide5.labelTitle.text = "A real-life bear"
+        slide5.labelDesc.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+        return [slide1, slide2, slide3, slide4, slide5]
+    }
+    
+    
+    func setupSlideScrollView(slides : [Slide]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+    }
+    
+    
+    /*
+     * default function called when view is scolled. In order to enable callback
+     * when scrollview is scrolled, the below code needs to be called:
+     * slideScrollView.delegate = self or
+     */
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+    }
+}
+
